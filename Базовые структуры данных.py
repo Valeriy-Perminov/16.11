c30d9ce2527ab0c09b51365bc7afe01fecb20aa5
Валерий <perminov.ahura999@gmail.com>
2024-11-21 19:01:25 +0500

    Add files via upload

---
+++ Базовые структуры данных.py
@@ -0,0 +1,19 @@
+#1st program
+a = int(9**0.5*5)
+print(a)
+
+#2nd program
+b = 99.9 > 99.8 and 1000 != 1000.1
+print(b)
+
+#3rd program
+c = 2*2+2 != (2*2)+2
+print(c)
+
+#4th program
+num_str = '123.456'
+num_float = float(num_str)
+num_float *= 10
+num_int = int(num_float)
+first_digit_after_point = num_int % 10
+print(first_digit_after_point)
